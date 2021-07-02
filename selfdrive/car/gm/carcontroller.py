aa1158c872d593049cf6b0199c826cbfd3ab91ca
hanabi95 <youngsoo.cha@gmail.com>
2021-07-02 15:04:02 +0900

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -51,26 +51,26 @@ class CarController():
       can_sends.append(gmcan.create_steering_control(self.packer_pt, CanBus.POWERTRAIN, apply_steer, idx, lkas_enabled))
 
     # Pedal/Regen
-    if CS.CP.enableGasInterceptor and (frame % 3) == 0:
+    if CS.CP.enableGasInterceptor and (frame % 2) == 0:
 
       if not enabled or not CS.adaptive_Cruise:
         final_pedal = 0
         #regen_active = False
       elif CS.adaptive_Cruise:
         #regen_active = True if actuators.brake > 0.01 else False
-        regen = clip(actuators.brake / 4, 0., 0.1)
-        Delta = actuators.gas - self.apply_pedal_last
+        regen = clip(actuators.brake / 2, 0., 0.1)
+        #Delta = actuators.gas - self.apply_pedal_last
         #min_pedal_speed = interp(CS.out.vEgo, VEL, MIN_PEDAL)
-        if Delta > 0:
-          pedal = 0.6 * actuators.gas + self.apply_pedal_last * 0.4
-        else:
-          pedal = self.apply_pedal_last + Delta / 10.
+        #if Delta > 0:
+        pedal = 0.6 * actuators.gas + self.apply_pedal_last * 0.4
+        #else:
+          #pedal = self.apply_pedal_last + Delta / 5.
 
-        #final_pedal = clip(pedal, min_pedal_speed, 1.)
-        final_pedal = clip(pedal - regen, 0., 1.)
+        gas_pedal = clip(pedal, .1, 1.)
+        final_pedal = clip(gas_pedal - regen, 0., 1.)
 
       self.apply_pedal_last = final_pedal
-      idx = (frame // 3) % 4
+      idx = (frame // 2) % 4
       can_sends.append(create_gas_command(self.packer_pt, final_pedal, idx))
 
     # Send dashboard UI commands (ACC status), 25hz
