404bd19cd2d86499675703ef1509c79aa7aa16b8
hanabi95 <61353425+hanabi95@users.noreply.github.com>
2021-07-03 10:22:59 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -501,7 +501,7 @@ class Controls:
 
     return actuators, lac_log
 
-  def publish_logs(self, CS, start_time, actuators, v_acc, a_acc, lac_log):
+  def publish_logs(self, CS, start_time, actuators, lac_log):
     """Send actuators and hud commands to the car, send controlsstate and MPC logging"""
 
     CC = car.CarControl.new_message()
